5344207ed2935e411127b6dbbfb7c61c1558c351
arhantalwar <arhantalwar@gmail.com>
2024-06-01 18:45:18 +0530

    add clone

--- src/main.rs
+++ src/main.rs
@@ -47,28 +47,6 @@ fn compile(path: &Path) {
                                 spawn().
                                 unwrap();
                         }
-                        "PYTHON" => {
-                            Command::new("python").
-                                current_dir(cur_dir).
-                                arg("main.py").
-                                spawn().
-                                unwrap();
-                        }
-
-                        "JS" => {
-                            Command::new("node").
-                                current_dir(cur_dir).
-                                arg("app.js").
-                                spawn().
-                                unwrap();
-                        }
-                        "JAVA" => {
-                            Command::new("java").
-                                current_dir(cur_dir).
-                                arg("main.java").
-                                spawn().
-                                unwrap();
-                        }
 
                         _ => {   }
 
@@ -84,13 +62,35 @@ fn compile(path: &Path) {
 
 }
 
-fn git_clone() {
+fn git_clone(path: &Path) {
+
+    let string_repos = fs::read_to_string("/home/arhant/Sandbox/rusty/jit_bot/src/git_repo_list").unwrap();
+    let mut list_of_repos = string_repos.split("\n").collect::<Vec<&str>>();
+    list_of_repos.pop();
+
+    for (num, url) in list_of_repos.iter().enumerate() {
+
+        let mut parent_path = String::from(path.to_str().unwrap());
+        parent_path.push_str(&num.to_string());
+
+        let temp_path = Path::new(&parent_path);
+        fs::create_dir(temp_path).unwrap();
+
+        Command::new("git")
+            .args(["clone", url, "."])
+            .current_dir(temp_path)
+            .spawn()
+            .unwrap();
+
+    }
+
 
 }
 
 fn main() {
 
-    compile(Path::new("/home/arhant/JIT_CUP/playground/"));
+    git_clone(Path::new("/home/arhant/JIT_CUP/playground/"));
+    // compile(Path::new("/home/arhant/JIT_CUP/playground/"));
 
 
 }
